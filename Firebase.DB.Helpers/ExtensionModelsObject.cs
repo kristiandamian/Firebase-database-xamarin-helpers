2e0039f33d974decc06578fc1a0dd87c66d70894
Kristian Damian <kristiandamian@gmail.com>
2017-08-30 13:05:48 -0600

    commit inicial

---
+++ Firebase.DB.Helpers/ExtensionModelsObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Firebase.DB.Helpers.Models;
+using Foundation;
+
+namespace Firebase.DB.Helpers
+{
+    public static class ExtensionModelsObject
+    {
+		/// <summary>
+		/// Genero un NSDictionary de un modelo que derive de la clase FirebaseBaseObjectModel para guardarlo en firebase de un jalon
+		/// </summary>
+		/// <returns>Las propiedades del objeto y sus valores como un NSDictionary</returns>
+		/// <param name="modelo"> instancia del tipo FirebaseBaseObjectModel</param>
+		public static NSDictionary ToNSDictionary(this FirebaseBaseObjectModel modelo)
+		{
+			var keys = new List<NSObject>();
+			var values = new List<NSObject>();
+			var type = modelo.GetType();
+			var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+			foreach (var pi in properties)
+			{
+				var selfValue = type.GetProperty(pi.Name).GetValue(modelo, null);
+
+				keys.Add(NSObject.FromObject(pi.Name));
+
+				if (selfValue != null)
+				{
+					values.Add(NSObject.FromObject(selfValue));
+				}
+				else
+				{
+					values.Add(NSObject.FromObject(null));
+				}
+			}
+
+			return NSDictionary.FromObjectsAndKeys(values.ToArray(), keys.ToArray(), keys.Count); ;
+		}
+
+		public static T ToFirebaseBaseModel<T>(this NSDictionary data) where T : new()
+		{
+			T tha_model = new T();
+			var type = tha_model.GetType();
+			var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+			foreach (var pi in properties)
+			{
+				PropertyInfo propertyInfo = type.GetProperty(pi.Name);
+				var value = data.ValueForKey((NSString)pi.Name);
+				//
+				try
+				{
+					var convertedValue = Convert.ChangeType(value.ToObject(propertyInfo.PropertyType), propertyInfo.PropertyType);
+
+					propertyInfo.SetValue(tha_model, convertedValue);
+				}
+				catch (Exception)
+				{
+					propertyInfo.SetValue(tha_model, null);
+				}
+			}
+			return tha_model;
+		}
+    }
+}
---
+++ Firebase.DB.Helpers/ExtensionsDateGenericas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Foundation;
+
+namespace Firebase.DB.Helpers
+{
+    public static class ExtensionsDateGenericas
+    {
+        public static NSDate ToNSDate(this DateTime date)
+        {
+            DateTime reference = TimeZone.CurrentTimeZone.ToLocalTime(
+                new DateTime(2001, 1, 1, 0, 0, 0));
+            return NSDate.FromTimeIntervalSinceReferenceDate(
+                (date - reference).TotalSeconds);
+        }
+
+        public static DateTime ToDateTime(this NSDate date)
+        {
+            DateTime reference = TimeZone.CurrentTimeZone.ToLocalTime(
+                new DateTime(2001, 1, 1, 0, 0, 0));
+            return reference.AddSeconds(date.SecondsSinceReferenceDate);
+        }
+    }
+}
